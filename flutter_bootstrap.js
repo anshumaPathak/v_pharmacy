8ff51d90a36b9e011b982b23dbd629de289b0390
anshumaP <anshumapathak2004@gmail.com>
2025-10-09 11:48:52 +0530

    Web build update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "531835497"
+    serviceWorkerVersion: "3510171086"
   }
 });
