303b9fb0cd792d080e37065796db237a97c14623
anshumaP <anshumapathak2004@gmail.com>
2025-10-09 09:23:08 +0530

    Add web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "04a103ae34902e3c94147d9f7e60e479
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e78cf2e64c3d7f20335de63154e10efa",
+"flutter_bootstrap.js": "c50f16608831821949443d2a84d1265d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
